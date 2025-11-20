fce7655e489bcc04deb4b03fc4dcc8acd84e654c
freddievo1399 <freddie.vo.1399@gmail.com>
2025-11-20 16:55:23 +0700

    Add project files.

---
+++ DemoChatSignalR.Server/Hub/HubChatRoom.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace DemoChatSignalR.Server
+{
+    public class HubChatRoom : Hub
+    {
+    }
+}
---
+++ DemoChatSignalR.Server/Service/CacheChatService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System.Collections.Concurrent;
+
+namespace DemoChatSignalR.Server
+{
+    public class CacheChatService(IMemoryCache memoryCache)
+    {
+        private IMemoryCache MemoryCache => memoryCache;
+        private static readonly ConcurrentDictionary<Guid, SemaphoreSlim> _roomLocks = new();
+    }
+}

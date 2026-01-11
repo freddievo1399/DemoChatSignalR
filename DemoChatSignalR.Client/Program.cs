3a845490b900d79402c3b10ca5650e66f1870c35
freddievo1399 <freddie.vo.1399@gmail.com>
2026-01-11 13:19:39 +0700

    update base address httpClient

--- DemoChatSignalR.Client/Program.cs
+++ DemoChatSignalR.Client/Program.cs
@@ -8,11 +8,11 @@ namespace DemoChatSignalR.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            var baseAddress = builder.Configuration["BaseAddress"]??"";
+            Console.WriteLine($"Base address: {baseAddress}");
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
             await builder.Build().RunAsync();
         }
     }
